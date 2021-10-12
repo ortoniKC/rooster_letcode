ad9f9a1828238fc76dd3e2209e78ee604ae8c877
Koushik Chatterjee <koushik350@gmail.com>
2021-10-12 21:08:23 +0530

    📦 NEW: init

---
+++ android/app/src/main/kotlin/com/example/rooster_letcode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rooster_letcode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+// import 'package:rooster_letcode/views/home.dart';
+// import 'package:rooster_letcode/views/login.dart';
+// import 'package:rooster_letcode/views/playwright.dart';
+
+// class AppRoutes {
+//   AppRoutes._();
+
+//   static const String authLogin = '/login';
+//   static const String home = '/home';
+//   static const String playwright = '/playwright';
+
+//   static Map<String, WidgetBuilder> define() {
+//     return {
+//       authLogin: (context) => Login(),
+//       home: (context) => Register(),
+//       playwright: (context) => MenuScreen(),
+//     };
+//   }
+// }
